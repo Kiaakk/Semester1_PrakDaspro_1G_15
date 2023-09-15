3e1eab8ff359ae38b46a4bc955e3a5b368ea00c5
Kiaakk <krisna.wiliam26@gmail.com>
2023-09-15 14:44:20 +0700

    P3_percobaan2 pertanyaan 3_15

--- src/Minggu3/ContohTipeData15.java
+++ src/Minggu3/ContohTipeData15.java
@@ -5,7 +5,7 @@ public class ContohTipeData15 {
         char golonganDarah = 'A';
         byte jarak = (byte) 130;
         short jumlahPendudukDalamSatuDusun = 1025;
-        float suhu = 60.50;
+        float suhu = 60.50F;
         double berat = 0.5467812345;
         long saldo = 150000000;
         int angkaDesimal = 0x10;
