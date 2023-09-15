407cd36fad497bc5b2d8e79f046d66f2ae4e6d03
Kiaakk <krisna.wiliam26@gmail.com>
2023-09-15 13:59:42 +0700

    P3_percobaan 1_pertanyaan 1_15

---
+++ src/Minggu3/Variabel15.java
@@ -0,0 +1,17 @@
+package Minggu3;
+public class Variabel15 {
+
+    public static void main(String[] args) {
+        String hobbySaya = "Bermain petak umpet";
+        boolean isPinter = true;
+        char jenisKelamin = 'L';
+        byte _umur = 20;
+        double $ipk = 3.24, tinggi = 1.78;
+        System.out.println(hobbySaya);
+        System.out.println("Apakah pandai? " + isPinter);
+        System.out.println("Jenis kelamin: " + jenisKelamin);
+        System.out.println("Umurku saat ini " + _umur);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+
+    }
+}
