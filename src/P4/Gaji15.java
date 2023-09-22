e35657f5933675e239d5ca363640827f5e132c09
Kiaakk <krisna.wiliam26@gmail.com>
2023-09-22 14:56:48 +0700

    P4 Percobaan2 Pertanyaan2 15

--- src/P4/Gaji15.java
+++ src/P4/Gaji15.java
@@ -9,12 +9,16 @@ public class Gaji15 {
         Scanner sc15 = new Scanner(System.in);
 
         int jmlMasuk, JmlTdkMasuk, TotGaji;
-        int gaji = 40000, potGaji = 25000;
+        int gaji, potGaji;
 
         System.out.print("Masukkan Jumlah Hari Masuk Kerja Anda ");
         jmlMasuk = sc15.nextInt();
         System.out.print("Masukkan Jumlah Hari Tidak Masuk Kerja Anda ");
         JmlTdkMasuk = sc15.nextInt();
+        System.out.print("Masukkan Jumlah Gaji ");
+        gaji = sc15.nextInt();
+        System.out.print("Masukkan Jumlah Potongan Gaji ");
+        potGaji = sc15.nextInt();
 
         TotGaji = (jmlMasuk*gaji)-(JmlTdkMasuk*potGaji);
 
