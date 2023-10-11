2bae9275578bb38d11da3b0ac9cd47403d4e656d
Kiaakk <krisna.wiliam26@gmail.com>
2023-10-11 21:03:46 +0700

    Pemilihan 2 Percobaan 1 Pertanyaan 4 _ 15

--- src/P6/Pemilihan2Percobaan1_15.java
+++ src/P6/Pemilihan2Percobaan1_15.java
@@ -13,16 +13,19 @@ public class Pemilihan2Percobaan1_15 {
         tahun = input15.nextInt();
 
         if ((tahun % 4) == 0) {
-
-            if ((tahun % 100) != 0) {
-                System.out.println("Tahun Kabisat");
+            if ((tahun % 100) == 0) {
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
             } else {
-                System.out.println("Bukan Tahun Kabisat");
+                System.out.println("Tahun Kabisat");
             }
-            
         } else {
             System.out.println("Bukan Tahun Kabisat");
         }
     }
 }
+
 
