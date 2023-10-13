a55f31e96ad98822ff7514a2455361a7b523761e
Kiaakk <krisna.wiliam26@gmail.com>
2023-10-13 13:42:49 +0700

    Perulangan 1 Percobaan 1 _ 15

---
+++ src/P7/ForKelipatan15.java
@@ -0,0 +1,27 @@
+package P7;
+
+import java.util.Scanner;
+
+public class ForKelipatan15 {
+
+    public static void main(String[] args) {
+        
+        Scanner input15 = new Scanner(System.in);
+
+        int kelipatan, jumlah=0, counter=0;
+
+        System.out.print("Masukkan bilangan kelipatan (1-9) : ");
+        kelipatan = input15.nextInt();
+
+        for (int i=1;i<=50;i++) {
+            if (i%kelipatan == 0) {
+                jumlah += i;
+                counter ++;
+            }
+        }
+
+        System.out.println("Banyaknya bilangan " + kelipatan + " dari 1 sampai 50 adalah " + counter);
+        System.out.println("Jumlah bilangan kelipatan " + kelipatan + " dari 1 sampai 50 adalah " + jumlah);
+    
+    }
+}
