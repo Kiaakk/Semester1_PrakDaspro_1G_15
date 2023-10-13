12802b7694943c40ca39d977f22d0d146ee59e90
Kiaakk <krisna.wiliam26@gmail.com>
2023-10-13 13:45:51 +0700

    Perulangan 1 Percobaan 1 Pertanyaan 3 _ 15

--- src/P7/ForKelipatan15.java
+++ src/P7/ForKelipatan15.java
@@ -9,6 +9,7 @@ public class ForKelipatan15 {
         Scanner input15 = new Scanner(System.in);
 
         int kelipatan, jumlah=0, counter=0;
+        double rataRata = 0;
 
         System.out.print("Masukkan bilangan kelipatan (1-9) : ");
         kelipatan = input15.nextInt();
@@ -17,11 +18,12 @@ public class ForKelipatan15 {
             if (i%kelipatan == 0) {
                 jumlah += i;
                 counter ++;
+                rataRata = (double)jumlah/counter;
             }
         }
 
         System.out.println("Banyaknya bilangan " + kelipatan + " dari 1 sampai 50 adalah " + counter);
         System.out.println("Jumlah bilangan kelipatan " + kelipatan + " dari 1 sampai 50 adalah " + jumlah);
-    
+        System.out.println("Rata-rata dari seluruh bilangan kelipatan " + kelipatan + " adalah " + rataRata);
     }
 }
