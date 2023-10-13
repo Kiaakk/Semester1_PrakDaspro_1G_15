eb1c6ada6444e13bc0fc789987bd26e4b573dddc
Kiaakk <krisna.wiliam26@gmail.com>
2023-10-13 14:40:29 +0700

    Perulangan 1 Percobaan 3 _ 15

---
+++ src/P7/DoWhileCuti15.java
@@ -0,0 +1,35 @@
+package P7;
+
+import java.util.Scanner;
+
+public class DoWhileCuti15 {
+    
+    public static void main(String[] args) {
+        
+        Scanner input15 = new Scanner(System.in);
+
+        int jatahCuti, jumlahHari;
+        String konfirmasi; 
+
+        System.out.print("Jatah cuti : ");
+        jatahCuti = input15.nextInt();
+
+        do {
+            System.out.print("Apakah anda ingin mengambil cuti (y/n) ? ");
+            konfirmasi = input15.next();
+
+            if (konfirmasi.equalsIgnoreCase(konfirmasi)) {
+                System.out.print("Jumlah hari : ");
+                jumlahHari = input15.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti " + jatahCuti);
+                } else {
+                    System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                    break;
+                }
+            }
+        } while (jatahCuti > 0);
+    }
+}
