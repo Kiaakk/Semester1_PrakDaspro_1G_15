2a96c51a606ce97009394cd9b439598a346e9bb1
Kiaakk <krisna.wiliam26@gmail.com>
2023-10-27 13:05:59 +0700

    Array 1 Percobaan 1 _ 15

---
+++ src/P9/ArrayBilangan15.java
@@ -0,0 +1,18 @@
+package P9;
+
+public class ArrayBilangan15 {
+    public static void main(String[] args) {
+        int[] bil = new int[4];
+
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}
+
