0f1b599f3b74d4f604a11bb5e19f9f04efeba56f
Kiaakk <krisna.wiliam26@gmail.com>
2023-10-31 23:09:50 +0700

    Array 1 Percobaan 2 Pertanyaan 3 _ 15

--- src/P9/ArrayNilai15.java
+++ src/P9/ArrayNilai15.java
@@ -12,8 +12,10 @@ public class ArrayNilai15 {
             System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = input15.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }
         }
 
     }
