16b106e84106158ecfcd4767057bfdd82e5556fd
Kiaakk <krisna.wiliam26@gmail.com>
2023-10-31 23:13:57 +0700

    Array 1 Percobaan 2 Pertanyaan 4 _ 15

--- src/P9/ArrayNilai15.java
+++ src/P9/ArrayNilai15.java
@@ -15,6 +15,8 @@ public class ArrayNilai15 {
         for (int i = 0; i < nilaiAkhir.length; i++) {
             if (nilaiAkhir[i] > 70) {
                 System.out.println("Mahasiswa ke-"+i+" lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
             }
         }
 
