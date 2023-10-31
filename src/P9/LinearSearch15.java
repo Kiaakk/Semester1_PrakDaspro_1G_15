65599444f3388a9a0f3fbf1bc89529a61a1b3043
Kiaakk <krisna.wiliam26@gmail.com>
2023-11-01 00:18:38 +0700

    Array 1 Percobaan 4 _ 15

---
+++ src/P9/LinearSearch15.java
@@ -0,0 +1,19 @@
+package P9;
+
+public class LinearSearch15 {
+    public static void main(String[] args) {
+        
+        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
+        int key = 20;
+        int hasil = 0;
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
+                hasil = i;
+                break;
+            }
+        }
+        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+
+    }
+}
