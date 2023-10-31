8e3bf84bcebc30630b24577a3859c83211de8df4
Kiaakk <krisna.wiliam26@gmail.com>
2023-10-31 22:31:57 +0700

    Array 1 Percobaan 1 Pertanyaan 2 _ 15

--- src/P9/ArrayBilangan15.java
+++ src/P9/ArrayBilangan15.java
@@ -2,12 +2,7 @@ package P9;
 
 public class ArrayBilangan15 {
     public static void main(String[] args) {
-        int[] bil = new int[4];
-        
-        bil[0] = 5.0;
-        bil[1] = 12867;
-        bil[2] = 7.5;
-        bil[3] = 2000000;
+        double[] bil = {5.0, 12867, 7.5, 2000000};
 
         System.out.println(bil[0]);
         System.out.println(bil[1]);
