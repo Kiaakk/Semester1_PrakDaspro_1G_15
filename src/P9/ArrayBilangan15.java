61283f83f6e89e6cf270e93e762b7e5aa487ab6d
Kiaakk <krisna.wiliam26@gmail.com>
2023-10-31 22:34:51 +0700

    Array 1 Percobaan 1 Pertanyaan 3 _ 15

--- src/P9/ArrayBilangan15.java
+++ src/P9/ArrayBilangan15.java
@@ -4,10 +4,9 @@ public class ArrayBilangan15 {
     public static void main(String[] args) {
         double[] bil = {5.0, 12867, 7.5, 2000000};
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i < bil.length; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
 
