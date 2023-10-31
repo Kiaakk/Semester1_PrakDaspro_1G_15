9701c9ab87f7c7a6a363e1c4c9a8c41b35f06acd
Kiaakk <krisna.wiliam26@gmail.com>
2023-11-01 00:19:53 +0700

    Array 1 Percobaan 4 _ 15

--- src/P9/LinearSearch15.java
+++ src/P9/LinearSearch15.java
@@ -2,7 +2,7 @@ package P9;
 
 public class LinearSearch15 {
     public static void main(String[] args) {
-        
+    
         int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
         int key = 20;
         int hasil = 0;
