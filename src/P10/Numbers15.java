0b598f4065681215e5fb244f5f9f1e0432074050
Kiaakk <krisna.wiliam26@gmail.com>
2023-11-10 17:03:49 +0700

    Array 2 Percobaan 3 _ 15

---
+++ src/P10/Numbers15.java
@@ -0,0 +1,10 @@
+package P10;
+
+public class Numbers15 {
+    public static void main(String[] args) {
+        int[][] myNumbers = new int[3][5];
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[3]; 
+        myNumbers[2] = new int[1];
+    }
+}
