fee92079cf3472172524aa9c551c7442f14340e5
Kiaakk <krisna.wiliam26@gmail.com>
2023-11-10 13:32:46 +0700

    Array 2 Percobaan 1 Pertanyaan 11 _ 15

--- src/P10/Bioskop15.java
+++ src/P10/Bioskop15.java
@@ -19,20 +19,14 @@ public class Bioskop15 {
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
         System.out.println();
 
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
-        }
-        System.out.println(
-
-        );
         for (String[] barisPenonton : penonton) {
             System.out.println("Panjang baris : " + barisPenonton.length);
         }
         System.out.println();
 
         System.out.println("Penonton pada baris ke-3 : ");
-        for (int i = 0; i < penonton[2].length; i++) {
-            System.out.println(penonton[2][i]);
+        for (String i : penonton[2]) {
+            System.out.println(i);
         }
 
     }
