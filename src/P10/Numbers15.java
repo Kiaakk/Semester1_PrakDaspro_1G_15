5ca14dfabed2465dfca1b71952d3914e46f1c60e
Kiaakk <krisna.wiliam26@gmail.com>
2023-11-10 17:11:02 +0700

    Array 2 Percobaan 3 Pertanyaan 1 _ 15

--- src/P10/Numbers15.java
+++ src/P10/Numbers15.java
@@ -1,10 +1,16 @@
 package P10;
 
+import java.util.Arrays;
+
 public class Numbers15 {
     public static void main(String[] args) {
         int[][] myNumbers = new int[3][5];
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3]; 
         myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
     }
 }
