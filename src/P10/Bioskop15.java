ca9adac3ecc5e82e96022e30822fae12c1e760ac
Kiaakk <krisna.wiliam26@gmail.com>
2023-11-10 12:47:09 +0700

    Array 2 Percobaan 1 Pertanyaan 5 _ 15

--- src/P10/Bioskop15.java
+++ src/P10/Bioskop15.java
@@ -19,10 +19,13 @@ public class Bioskop15 {
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
         System.out.println(penonton.length);
-        System.out.println(penonton[0].length);
-        System.out.println(penonton[1].length);
-        System.out.println(penonton[2].length);
-        System.out.println(penonton[3].length);
+        // System.out.println(penonton[0].length);
+        // System.out.println(penonton[1].length);
+        // System.out.println(penonton[2].length);
+        // System.out.println(penonton[3].length);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+        }
     
     }
 }
