8a12a06e154b41c0a124a8706eac9d9c65ed44ac
Kiaakk <krisna.wiliam26@gmail.com>
2023-11-13 19:42:07 +0700

    Perulangan 2 Percobaan 2 Pertanyaan 1 _ 15

--- src/P11/Square15.java
+++ src/P11/Square15.java
@@ -10,7 +10,7 @@ public class Square15 {
         System.out.print("Masukkan nilai N = ");
         int N = sc15.nextInt();
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
                 System.out.print("* ");
             }
