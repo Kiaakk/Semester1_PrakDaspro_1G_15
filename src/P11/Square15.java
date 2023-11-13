223d9bc7628f93105fc88973da5b603c55674bf8
Kiaakk <krisna.wiliam26@gmail.com>
2023-11-13 19:37:14 +0700

    Perulangan 2 Percobaan 2 _ 15

---
+++ src/P11/Square15.java
@@ -0,0 +1,20 @@
+package P11;
+
+import java.util.Scanner;
+
+public class Square15 {
+    public static void main(String[] args) {
+        
+        Scanner sc15 = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N = ");
+        int N = sc15.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
