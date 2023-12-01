a8100b86a2418c57e1729da955749a3e324b0e9b
Kiaakk <krisna.wiliam26@gmail.com>
2023-12-01 10:04:21 +0700

    Fungsi 1 Percobaan 2 _ 15

--- src/P13/TerimaKasih_15.java
+++ src/P13/TerimaKasih_15.java
@@ -3,12 +3,16 @@ package P13;
 public class TerimaKasih_15 {
     public static void main(String[] args) {
         UcapanTerimaKasih();
-        System.out.println("Thank you for being the best teacher in the world.\n" + 
-        "You inspired in me a love for learing and made me feel like I could ask you anything.");
+        String ucapan = "Terima kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan); 
     }
 
     public static void UcapanTerimaKasih() {
         System.out.println("Thank you for being the best teacher in the world.\n" + 
         "You inspired in me a love for learing and made me feel like I could ask you anything.");
+    }
+
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
     }
 }
